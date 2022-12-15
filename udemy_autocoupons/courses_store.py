ca6721643a4000b52770c48f0c5b3eee346f2c4b
Pietro Palombini <pietropalo@icloud.com>
2022-12-14 21:45:25 -0300

    Optimize CoursesStore performance

--- udemy_autocoupons/courses_store.py
+++ udemy_autocoupons/courses_store.py
@@ -1,5 +1,6 @@
 """This module contains the CoursesStore class."""
 from collections.abc import Iterator, MutableSet
+from dataclasses import astuple
 
 from udemy_autocoupons.udemy_course import (
     CourseWithAnyCoupon,
@@ -20,29 +21,24 @@ class CoursesStore(MutableSet):
 
     def __init__(self, *args: UdemyCourseT) -> None:
         """Adds all passed courses to the store."""
+        # The key is the url_id
+        self._any_coupon: dict[str, CourseWithAnyCoupon] = {}
         self._specific_coupon: set[CourseWithCoupon] = set()
-        self._any_coupon: set[CourseWithAnyCoupon] = set()
 
         for course in args:
             self.add(course)
 
     def __contains__(self, course: UdemyCourseT) -> bool:
         """Checks if the given element is in the store."""
-        if course.any_coupon:
-            return course in self._any_coupon
-
-        if course in self._specific_coupon:
+        if course.url_id in self._any_coupon:
             return True
 
-        return any(
-            stored_course.url_id == course.url_id
-            for stored_course in self._any_coupon
-        )
+        return False if course.any_coupon else course in self._specific_coupon
 
     def __iter__(self) -> Iterator[UdemyCourseT]:
-        """Iterates first over the courses with any_coupon==True."""
-        yield from self._any_coupon
+        """Iterates first over the courses with a specific coupon."""
         yield from self._specific_coupon
+        yield from self._any_coupon.values()
 
     def __len__(self) -> int:
         """Returns the number of items in the store.
@@ -65,7 +61,8 @@ class CoursesStore(MutableSet):
             repr_ += f'{specific_repr}, '
 
         if self._any_coupon:
-            repr_ += repr(self._any_coupon)[1:-1]
+            any_coupon_set = set(self._any_coupon.values())
+            repr_ += repr(any_coupon_set)[1:-1]
 
         repr_ += ')'
 
@@ -73,8 +70,11 @@ class CoursesStore(MutableSet):
 
     def add(self, course: UdemyCourseT) -> None:
         """Adds a course to the store."""
+        if course in self:
+            return
+
         if is_with_any_coupon(course):
-            self._any_coupon.add(course)
+            self._any_coupon[course.url_id] = course
         elif is_with_specific_coupon(course):
             self._specific_coupon.add(course)
         else:
@@ -82,8 +82,8 @@ class CoursesStore(MutableSet):
 
     def discard(self, course: UdemyCourseT) -> None:
         """Removes a course without raising if it doesn't exist."""
-        if is_with_any_coupon(course):
-            self._any_coupon.discard(course)
+        if is_with_any_coupon(course) and course.url_id in self._any_coupon:
+            self._any_coupon.pop(course.url_id)
         elif is_with_specific_coupon(course):
             self._specific_coupon.discard(course)
         else:
@@ -91,7 +91,41 @@ class CoursesStore(MutableSet):
 
     def optimize(self) -> None:
         """Optimizes the memory usage by removing redundant courses."""
-        for any_coupon in self._any_coupon:
-            for specific_coupon in self._specific_coupon:
-                if specific_coupon.url_id == any_coupon.url:
-                    self._specific_coupon.remove(specific_coupon)
+        for specific_coupon in self._specific_coupon:
+            if specific_coupon.url_id in self._any_coupon:
+                self._specific_coupon.remove(specific_coupon)
+
+    def create_compressed(self) -> tuple[str | tuple[str, str], ...]:
+        """Creates a smaller representation of the store.
+
+        The store can be recreated by using load_compressed with the return
+        value of this method.
+
+        Side effect: the store is optimized.
+
+        Returns:
+            The compressed representation.
+
+        """
+        self.optimize()
+        return tuple(self._any_coupon.keys()) + tuple(
+            astuple(specific_coupon)
+            for specific_coupon in self._specific_coupon
+        )
+
+    def load_compressed(
+        self,
+        compressed: tuple[str | tuple[str, str], ...],
+    ) -> None:
+        """Loads courses into the store from a compressed representation.
+
+        Args:
+            compressed: The compressed representation, as returned by
+            create_compressed.
+
+        """
+        for compressed_course in compressed:
+            if isinstance(compressed_course, str):
+                self.add(CourseWithAnyCoupon(compressed_course))
+            else:
+                self.add(CourseWithCoupon(*compressed_course))
