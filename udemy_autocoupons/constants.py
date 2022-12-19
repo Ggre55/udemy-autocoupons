9b019d4d73aa52c3522dd5e797327c44a703778c
Pietro Palombini <pietropalo@icloud.com>
2022-12-19 17:59:12 -0300

    Increase driver wait poll frequency

--- udemy_autocoupons/constants.py
+++ udemy_autocoupons/constants.py
@@ -1,6 +1,6 @@
 """Constants used across the project."""
 
 WAIT_TIMEOUT = 10
-WAIT_POLL_FREQUENCY = 0.25
+WAIT_POLL_FREQUENCY = 0.05
 PROFILE_DIRECTORY = 'Profile 1'
 USER_DATA_DIR = '~/.config/google-chrome/'
