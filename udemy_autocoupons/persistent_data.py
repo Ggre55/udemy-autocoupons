f3b2d79728ac0c533c708ff7fc0c7c5b0f141541
Pietro Palombini <pietropalo@icloud.com>
2023-04-08 17:06:33 -0300

    Refactor persistent_data to reduce duplication

--- udemy_autocoupons/persistent_data.py
+++ udemy_autocoupons/persistent_data.py
@@ -9,8 +9,6 @@ from typing import Any
 from udemy_autocoupons.courses_store import CoursesStore
 from udemy_autocoupons.scrapers import ScrapersT
 
-scrapers_path = Path.cwd() / "data" / "scrapers.pickle"
-courses_store_path = Path.cwd() / "data" / "courses_store.pickle"
 _debug = getLogger("debug")
 
 
@@ -21,21 +19,12 @@ def save_scrapers_data(scrapers: ScrapersT) -> None:
         scrapers: The used scrapers.
 
     """
-    if not scrapers_path.is_file():
-        scrapers_path.parent.mkdir(parents=True, exist_ok=True)
-        scrapers_path.touch()
-
     scrapers_data = {
         scraper.__class__.__name__: scraper.create_persistent_data()
         for scraper in scrapers
     }
 
-    _debug.debug("Saving %s", scrapers_data)
-
-    with scrapers_path.open("wb") as pickle_file:
-        pickler = Pickler(pickle_file, 4)
-
-        pickler.dump(scrapers_data)
+    _save_persistent("scrapers.pickle", scrapers_data)
 
 
 def load_scrapers_data() -> defaultdict[str, Any]:
@@ -46,18 +35,10 @@ def load_scrapers_data() -> defaultdict[str, Any]:
         to the amount of scrapers by adding None at the end if needed.
 
     """
-    if not scrapers_path.is_file():
-        _debug.debug("No file found in %s", scrapers_path)
+    if previous := _load_persistent("scrapers.pickle"):
+        return defaultdict(lambda: None, previous)
 
-        return defaultdict(lambda: None)
-
-    with scrapers_path.open("rb") as pickle_file:
-        pickler = Unpickler(pickle_file)
-        previous_persistent_data = pickler.load()
-
-        _debug.debug("Loaded %s", previous_persistent_data)
-
-        return defaultdict(lambda: None, previous_persistent_data)
+    return defaultdict(lambda: None)
 
 
 def save_courses_store(courses_store: CoursesStore) -> None:
@@ -67,14 +48,7 @@ def save_courses_store(courses_store: CoursesStore) -> None:
         courses_store: The courses store.
 
     """
-    if not courses_store_path.is_file():
-        courses_store_path.parent.mkdir(parents=True, exist_ok=True)
-        courses_store_path.touch()
-
-    with courses_store_path.open("wb") as pickle_file:
-        pickler = Pickler(pickle_file, 4)
-
-        pickler.dump(courses_store.create_compressed())
+    _save_persistent("courses_store.pickle", courses_store.create_compressed())
 
 
 def load_courses_store() -> CoursesStore:
@@ -84,18 +58,55 @@ def load_courses_store() -> CoursesStore:
         The courses store if it can be found, an empty one otherwise.
 
     """
-    if not courses_store_path.is_file():
-        _debug.debug("No file found in %s", courses_store_path)
+    courses_store = CoursesStore()
 
-        return CoursesStore()
-
-    with courses_store_path.open("rb") as pickle_file:
-        pickler = Unpickler(pickle_file)
-        compressed = pickler.load()
-
-        _debug.debug("Loaded %s", compressed)
-
-        courses_store = CoursesStore()
+    if compressed := _load_persistent("courses_store.pickle"):
         courses_store.load_compressed(compressed)
 
-        return courses_store
+    return courses_store
+
+
+def _save_persistent(filename: str, to_persist: Any) -> None:
+    """Saves the scrapers persistent data to a file.
+
+    Args:
+        filename: The filename to use. Data is always stored in the data dir.
+        to_persist: The data to persist.
+
+    """
+    path = Path.cwd() / "data" / filename
+    if not path.is_file():
+        path.parent.mkdir(parents=True, exist_ok=True)
+        path.touch()
+
+    _debug.debug("Saving %s", to_persist)
+
+    with path.open("wb") as pickle_file:
+        pickler = Pickler(pickle_file, 4)
+
+        pickler.dump(to_persist)
+
+
+def _load_persistent(filename: str) -> Any | None:
+    """Loads persistent data.
+
+    Args:
+        filename: The filename to use. The file is read from the data dir.
+
+    Returns:
+        The persistent data if it can be found, None otherwise.
+
+    """
+    path = Path.cwd() / "data" / filename
+    if not path.is_file():
+        _debug.debug("No file found in %s", path)
+
+        return None
+
+    with path.open("rb") as pickle_file:
+        pickler = Unpickler(pickle_file)
+        previous_persistent_data = pickler.load()
+
+        _debug.debug("Loaded %s", previous_persistent_data)
+
+        return previous_persistent_data
