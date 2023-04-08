4bc74663f038d3edf2479c56c9dde345c31567fa
Pietro Palombini <pietropalo@icloud.com>
2023-04-08 03:44:12 -0300

    Make CoursesStore thread-safe

--- udemy_autocoupons/courses_store.py
+++ udemy_autocoupons/courses_store.py
@@ -1,6 +1,7 @@
 """This module contains the CoursesStore class."""
 from collections.abc import Iterator, MutableSet
 from dataclasses import astuple
+from threading import RLock
 
 from udemy_autocoupons.udemy_course import (
     CourseWithAnyCoupon,
@@ -17,6 +18,9 @@ class CoursesStore(MutableSet):
     This store considers that if it contains a course with any_coupon, then all
     courses that have the same url_id are contained too.
 
+    Mutation methods are thread-safe. Reading do not use a mutex, so while it is
+    safe to read from multiple threads, the result might not be consistent.
+
     """
 
     def __init__(self, *args: UdemyCourseT) -> None:
@@ -25,8 +29,11 @@ class CoursesStore(MutableSet):
         self._any_coupon: dict[str, CourseWithAnyCoupon] = {}
         self._specific_coupon: set[CourseWithCoupon] = set()
 
-        for course in args:
-            self.add(course)
+        self._lock = RLock()
+
+        with self._lock:
+            for course in args:
+                self._add(course)
 
     def __contains__(self, course: UdemyCourseT) -> bool:
         """Checks if the given element is in the store."""
@@ -70,30 +77,20 @@ class CoursesStore(MutableSet):
 
     def add(self, course: UdemyCourseT) -> None:
         """Adds a course to the store."""
-        if course in self:
-            return
-
-        if is_with_any_coupon(course):
-            self._any_coupon[course.url_id] = course
-        elif is_with_specific_coupon(course):
-            self._specific_coupon.add(course)
-        else:
-            raise TypeError
+        with self._lock:
+            self._add(course)
 
     def discard(self, course: UdemyCourseT) -> None:
         """Removes a course without raising if it doesn't exist."""
-        if is_with_any_coupon(course) and course.url_id in self._any_coupon:
-            self._any_coupon.pop(course.url_id)
-        elif is_with_specific_coupon(course):
-            self._specific_coupon.discard(course)
-        else:
-            raise TypeError
+        with self._lock:
+            self._discard(course)
 
     def optimize(self) -> None:
         """Optimizes the memory usage by removing redundant courses."""
-        for specific_coupon in self._specific_coupon:
-            if specific_coupon.url_id in self._any_coupon:
-                self._specific_coupon.remove(specific_coupon)
+        with self._lock:
+            for specific_coupon in self._specific_coupon:
+                if specific_coupon.url_id in self._any_coupon:
+                    self._specific_coupon.remove(specific_coupon)
 
     def create_compressed(self) -> tuple[str | tuple[str, str], ...]:
         """Creates a smaller representation of the store.
@@ -107,11 +104,12 @@ class CoursesStore(MutableSet):
             The compressed representation.
 
         """
-        self.optimize()
-        return tuple(self._any_coupon.keys()) + tuple(
-            astuple(specific_coupon)
-            for specific_coupon in self._specific_coupon
-        )
+        with self._lock:
+            self.optimize()
+            return tuple(self._any_coupon.keys()) + tuple(
+                astuple(specific_coupon)
+                for specific_coupon in self._specific_coupon
+            )
 
     def load_compressed(
         self,
@@ -124,8 +122,30 @@ class CoursesStore(MutableSet):
             create_compressed.
 
         """
-        for compressed_course in compressed:
-            if isinstance(compressed_course, str):
-                self.add(CourseWithAnyCoupon(compressed_course))
-            else:
-                self.add(CourseWithCoupon(*compressed_course))
+        with self._lock:
+            for compressed_course in compressed:
+                if isinstance(compressed_course, str):
+                    self._add(CourseWithAnyCoupon(compressed_course))
+                else:
+                    self._add(CourseWithCoupon(*compressed_course))
+
+    def _add(self, course: UdemyCourseT) -> None:
+        """Adds a course to the store."""
+        if course in self:
+            return
+
+        if is_with_any_coupon(course):
+            self._any_coupon[course.url_id] = course
+        elif is_with_specific_coupon(course):
+            self._specific_coupon.add(course)
+        else:
+            raise TypeError
+
+    def _discard(self, course: UdemyCourseT) -> None:
+        """Removes a course without raising if it doesn't exist."""
+        if is_with_any_coupon(course) and course.url_id in self._any_coupon:
+            self._any_coupon.pop(course.url_id)
+        elif is_with_specific_coupon(course):
+            self._specific_coupon.discard(course)
+        else:
+            raise TypeError
