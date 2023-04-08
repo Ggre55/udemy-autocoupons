e54e2767e13b98d3fa41a69030147f430b25efc2
Pietro Palombini <pietropalo@icloud.com>
2023-04-08 03:45:17 -0300

    Implement saving and loading of CoursesStore

--- udemy_autocoupons/persistent_data.py
+++ udemy_autocoupons/persistent_data.py
@@ -6,9 +6,11 @@ from pathlib import Path
 from pickle import Pickler, Unpickler
 from typing import Any
 
+from udemy_autocoupons.courses_store import CoursesStore
 from udemy_autocoupons.scrapers import ScrapersT
 
 scrapers_path = Path.cwd() / "data" / "scrapers.pickle"
+courses_store_path = Path.cwd() / "data" / "courses_store.pickle"
 _debug = getLogger("debug")
 
 
@@ -58,4 +60,42 @@ def load_scrapers_data() -> defaultdict[str, Any]:
         return defaultdict(lambda: None, previous_persistent_data)
 
 
-load_scrapers_data()
+def save_courses_store(courses_store: CoursesStore) -> None:
+    """Saves the courses store to a file.
+
+    Args:
+        courses_store: The courses store.
+
+    """
+    if not courses_store_path.is_file():
+        courses_store_path.parent.mkdir(parents=True, exist_ok=True)
+        courses_store_path.touch()
+
+    with courses_store_path.open("wb") as pickle_file:
+        pickler = Pickler(pickle_file, 4)
+
+        pickler.dump(courses_store.create_compressed())
+
+
+def load_courses_store() -> CoursesStore:
+    """Loads the courses store.
+
+    Returns:
+        The courses store if it can be found, an empty one otherwise.
+
+    """
+    if not courses_store_path.is_file():
+        _debug.debug("No file found in %s", courses_store_path)
+
+        return CoursesStore()
+
+    with courses_store_path.open("rb") as pickle_file:
+        pickler = Unpickler(pickle_file)
+        compressed = pickler.load()
+
+        _debug.debug("Loaded %s", compressed)
+
+        courses_store = CoursesStore()
+        courses_store.load_compressed(compressed)
+
+        return courses_store
